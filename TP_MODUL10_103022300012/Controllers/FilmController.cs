c7de166448da77e8c27a7108f16250babc37f146
ibiinn <ibiinn05@gmail.com>
2026-05-04 00:14:24 +0700

    TP MODUL 10

---
+++ TP_MODUL10_103022300012/Controllers/FilmController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TP_MODUL10_103022300012.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmController : ControllerBase
+    {
+        private static List<Film> films = new List<Film>
+        {
+            new Film(1, "Inception", "Christopher Nolan", 2010, "Sci-Fi", 9.0),
+            new Film(2, "Interstellar", "Christopher Nolan", 2014, "Sci-Fi", 8.7),
+            new Film(3, "Parasite", "Bong Joon-ho", 2019, "Thriller", 8.6)
+        };
+
+        [HttpGet]
+        public ActionResult<List<Film>> GetAllFilms()
+        {
+            return Ok(films);
+        }
+
+        [HttpGet("{index}")]
+        public ActionResult<Film> GetFilmByIndex(int index)
+        {
+            return Ok(films[index]);
+        }
+
+        [HttpPost]
+        public ActionResult AddFilm([FromBody] Film film)
+        {
+            films.Add(film);
+            return Ok();
+        }
+
+        [HttpDelete("{index}")]
+        public ActionResult DeleteFilm(int index)
+        {
+            films.RemoveAt(index);
+            return Ok();
+        }
+    }
+}
---
+++ TP_MODUL10_103022300012/Film.cs
@@ -0,0 +1,22 @@
+﻿namespace TP_MODUL10_103022300012
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        public string Judul { get; set; }
+        public string Sutradara { get; set; }
+        public int Tahun { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+
+        public Film(int id, string judul, string sutradara, int tahun, string genre, double rating)
+        {
+            Id = id;
+            Judul = judul;
+            Sutradara = sutradara;
+            Tahun = tahun;
+            Genre = genre;
+            Rating = rating;
+        }
+    }
+}
